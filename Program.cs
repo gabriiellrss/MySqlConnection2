113a565ab1a1a87b4ff7acd4b5acd32c636b584b
gabriiellrss <gabriel.satana166@etec.sp.gov.br>
2024-10-09 17:12:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace LEGAL2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conexao;
+            MySqlConnection cmd;
+
+            conexao = new MySqlConnection();
+
+            try
+            {
+                string stringconexao = "server=127.0.0.1;uid=root;pwd='123';database=TESTE";
+                conexao.ConnectionString = stringconexao;
+                conexao.Open();
+                Console.WriteLine("conexão estabelecida");
+                Console.ReadKey();
+            }
+            catch (MySqlException ex)
+            {
+
+                if (ex.Number == 1045)
+                {
+                    Console.WriteLine("banco esta ativo");
+                }
+                Console.WriteLine("ERRO GERADO:"+ex.Number);
+                Console.WriteLine("ENtre em contato com o adm");
+                Console.ReadKey();
+
+            }
+            string sql = "select * from usuario;";
+            cmd = new MySqlCommand(sql, conexao);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("id:" + reader["id"];
+
+            }
+        }
+    }
+}
